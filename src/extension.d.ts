76da6f42b77ffcece31ce3801fc69ab0810e246b
Randolf J. <randolf@mathematic.io>
2025-01-28 19:41:27 +0900

    chore: remove build assets

--- src/extension.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ExtensionContext } from "vscode";
-export declare function activate(context: ExtensionContext): void;
-export declare function deactivate(): void;
-//# sourceMappingURL=extension.d.ts.map
